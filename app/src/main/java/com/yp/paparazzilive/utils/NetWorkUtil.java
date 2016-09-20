1f6c5243159019b6c02bb283b1f65f4c4db97a77
yp <834549931@qq.com>
2016-09-20 14:17:32 +0800

    框架的搭建

---
+++ app/src/main/java/com/yp/paparazzilive/utils/NetWorkUtil.java
@@ -0,0 +1,28 @@
+package com.yp.paparazzilive.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by yp on 2016/9/17.
+ */
+public class NetWorkUtil {
+
+    public static boolean isConnected(Context context){
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo!=null && networkInfo.getState() == NetworkInfo.State.CONNECTED) {
+
+            return true;
+        }
+
+        return false;
+
+    }
+
+
+}
